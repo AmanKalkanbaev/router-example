f0aaf8a8635045650f3014c57f0890d5ce4051ac
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-04-14 16:27:22 +0600

    Added 3 new components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Home from "./Home/Home"
+import About from "./About/About"
+import Contacts from "./Contacts/Contacts"
 
 function App() {
   return (
     <div className="App">
+      <Home/>
+      <About/>
+      <Contacts/>
       
     </div>
   );
