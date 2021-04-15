44db0b614318d4f7e6a1ef9e5baac821c2c815bb
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-04-16 03:28:03 +0600

    writed about content

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,27 @@
+import News from "../Media/News.png";
+
+import classes from "./About.module.css";
+
 const About = () => {
     return ( 
-        <div>
-            <h1>
-                About
-            </h1>
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I
-            </p>
+        <div className={classes.About}>
+            <div className={classes.header}>
+                <h1>
+                    We are Abizyana News
+                </h1>
+            </div>
+            <div className={classes.block}>
+                <p>
+                    Welcome to Abizyana News, your number one news source. We strive to provide you with the best news with an emphasis on Accuracy, Speed, Relevance. Founded in 2021 by Aman Kalkanbaev, Abizyana News has come a long way from its founding in Karakol. When Aman first started out, his passion for Tales pushed them to start their own business. We hope you enjoy our products as much as we do. If you have any questions or comments, do not hesitate to contact us. Best regards, Abizyana News
+                </p>
+                <img src={News}/>
+            </div>
+
+            
+            
+           
         </div>
-     );
+    );
 }
  
 export default About;
