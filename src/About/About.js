764a6ada2de0e3f9048b831cab9a88493758ef68
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-04-16 03:29:20 +0600

    added alt to the picture

--- src/About/About.js
+++ src/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
                 <p>
                     Welcome to Abizyana News, your number one news source. We strive to provide you with the best news with an emphasis on Accuracy, Speed, Relevance. Founded in 2021 by Aman Kalkanbaev, Abizyana News has come a long way from its founding in Karakol. When Aman first started out, his passion for Tales pushed them to start their own business. We hope you enjoy our products as much as we do. If you have any questions or comments, do not hesitate to contact us. Best regards, Abizyana News
                 </p>
-                <img src={News}/>
+                <img alt={"News"} src={News}/>
             </div>
 
             
